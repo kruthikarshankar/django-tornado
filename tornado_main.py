ee8314c92e06ad16ca116586559ea646541454f5
Kruthika Rathinavel <kruthika@vt.edu>
2015-02-23 10:33:26 -0500

    First commit:

---
+++ tornado_main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# Run this with
+# PYTHONPATH=. DJANGO_SETTINGS_MODULE=testsite.settings testsite/tornado_main.py
+# Serves by default at
+# http://localhost:8080/hello-django
+
+from tornado.options import options, define, parse_command_line
+import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.wsgi
+import os
+import sys
+
+define('port', type=int, default=8080)
+
+def main():
+    #parse_command_line()
+  os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+  wsgi_app = tornado.wsgi.WSGIContainer(
+    get_wsgi_application())
+  tornado_app = tornado.web.Application(
+    [
+      ('.*', tornado.web.FallbackHandler, dict(fallback=wsgi_app)),
+      ])
+  server = tornado.httpserver.HTTPServer(tornado_app)
+  server.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+  main()
